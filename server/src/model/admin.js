505519a88738bcbf53c620d10312bcadd5463be3
csabarato <=>
2020-06-27 14:26:51 +0200

    Add login endpoint for Admin model, using JWT auth

---
+++ server/src/model/admin.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const adminSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 7
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+})
+
+// Statics
+adminSchema.statics.findByCredentials = async function (email, password) {
+
+    const admin = await Admin.findOne({email: email})
+
+    if (!admin) {
+        throw new Error("No user found.")
+    }
+
+    const passwordMatch = bcrypt.compare(password, admin.password)
+
+    if (!passwordMatch) {
+        throw new Error("Invalid username or password")
+    }
+    return admin
+}
+
+
+// Methods
+adminSchema.methods.generateAuthToken = async function () {
+
+    const token = jwt.sign({id: this.id.toString}, process.env.JWT_SECRET)
+
+    this.tokens.push({token: token})
+    await this.save()
+
+    return token
+}
+
+
+
+
+
+const Admin = mongoose.model('Admin',adminSchema)
+module.exports = Admin;
+
+
--- server/src/router/adminRouter.js
+++ server/src/router/adminRouter.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const router = new express.Router()
+const Admin = require('../model/admin')
 
-router.get('/', (req, res) => {})
+router.post('/admin/login',async (req, res) => {
+
+    try {
+        const admin = await Admin.findByCredentials(req.body.email, req.body.password)
+
+        const token = await admin.generateAuthToken()
+        res.send({admin, token})
+
+    } catch (e) {
+       res.status(401).send({error: e.message})
+    }
+
+})
 
 
 module.exports = router
